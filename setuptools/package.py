b48be139d130775592cd015659bb571832471315
Hugo Parente Lima <hugo.pl@gmail.com>
2011-03-31 15:52:37 -0300

    Use whitespaces instead of tabs and add version info based on the current day.

--- setuptools/package.py
+++ setuptools/package.py
@@ -58,7 +58,7 @@ except ImportError:
                 errors.extend((src, dst, str(why)))
         if errors:
             raise Error, errors
-            
+
 def replace_in_file(src, dst, vars):
     f = open(src, "rt")
     content =  f.read()
@@ -79,34 +79,34 @@ def run_process(*args):
 
 def create_package(pkg_version, script_dir, output_dir, py_version, qtinfo, cleanup):
     print "Generating python distribution package..."
-    
+
     os.chdir(script_dir)
-    modules_dir = os.path.join(script_dir, "modules")   
-    
+    modules_dir = os.path.join(script_dir, "modules")
+
     pkgsrc_dir = os.path.join(script_dir, "src")
     if os.path.exists(pkgsrc_dir):
         print "Deleting folder %s..." % pkgsrc_dir
         shutil.rmtree(pkgsrc_dir, False)
     os.mkdir(pkgsrc_dir)
-    
+
     build_dir = os.path.join(script_dir, "build")
     if cleanup and os.path.exists(build_dir):
         print "Deleting folder %s..." % build_dir
         shutil.rmtree(build_dir, False)
-    
+
     # <output>/lib/site-packages/PySide/* -> src/PySide
     src = os.path.join(output_dir, "lib/site-packages/PySide")
     print "Copying PySide sources from %s" % (src)
     copytree(src, os.path.join(pkgsrc_dir, "PySide"))
-    
+
     # <output>/lib/site-packages/pysideuic/* -> src/pysideuic
     src = os.path.join(output_dir, "lib/site-packages/pysideuic")
     if os.path.exists(src):
         print "Copying pysideuic sources from %s" % (src)
         copytree(src, os.path.join(pkgsrc_dir, "pysideuic"))
     else:
-        print "Skiping pysideuic." 
-    
+        print "Skiping pysideuic."
+
     # <output>/bin/pyside-uic -> src/PySide/scripts/uic.py
     src = os.path.join(output_dir, "bin/pyside-uic")
     dst = os.path.join(pkgsrc_dir, "PySide/scripts")
@@ -120,7 +120,7 @@ def create_package(pkg_version, script_dir, output_dir, py_version, qtinfo, clea
         shutil.copy(src, dst)
     else:
         print "Skip uic file"
-    
+
     def cpbin(name):
         src = os.path.join(output_dir, "bin/%s" % name)
         if os.path.exists(src):
@@ -128,19 +128,19 @@ def create_package(pkg_version, script_dir, output_dir, py_version, qtinfo, clea
             shutil.copy(src, os.path.join(pkgsrc_dir, "PySide/%s" % name))
         else:
             print "Skiping %s..." % src
-    
+
     # <output>/bin/pyside-lupdate.exe -> src/PySide/pyside-lupdate.exe
     cpbin("pyside-lupdate.exe")
-    
+
     # <output>/bin/pyside-rcc.exe -> src/PySide/pyside-rcc.exe
     cpbin("pyside-rcc.exe")
-    
+
     # <output>/bin/pyside.dll -> src/PySide/pyside.dll
     cpbin("pyside-python%s.dll" % py_version)
-    
+
     # <output>/bin/shiboken.dll -> src/PySide/shiboken.dll
     cpbin("shiboken-python%s.dll" % py_version)
-    
+
     # <qt>/bin/* -> src/PySide
     src = qtinfo.bins_dir
     dst = os.path.join(pkgsrc_dir, "PySide")
@@ -153,38 +153,38 @@ def create_package(pkg_version, script_dir, output_dir, py_version, qtinfo, clea
             dstname = os.path.join(dst, name)
             print "Copying \"%s\"" % (srcname)
             shutil.copy(srcname, dstname)
-    
+
     # <qt>/plugins/* -> src/PySide/plugins
     src = qtinfo.plugins_dir
     print "Copying Qt plugins from %s" % (src)
     copytree(src, os.path.join(pkgsrc_dir, "PySide/plugins"), ignore=ignore_patterns('*.lib', '*d4.dll'))
-    
+
     # <modules>/examples/* -> src/PySide/examples
     src = os.path.join(modules_dir, "examples")
     if os.path.exists(src):
         print "Copying PySide examples from %s" % (src)
         copytree(src, os.path.join(pkgsrc_dir, "PySide/examples"))
-    
+
     # Prepare setup.py
     print "Preparing setup.py..."
     version_str = "%sqt%s" % (pkg_version, qtinfo.version.replace(".", "")[0:3])
     replace_in_file("setup.py.in", "setup.py", { "${version}": version_str })
-    
+
     # Build package
     print "Building distribution package..."
     if run_process(sys.executable, "setup.py", "bdist_wininst", "--target-version=%s" % py_version) != 0:
         raise Exception("Error building distribution package ")
-        
-if __name__ == "__main__":       
+
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print "Usage: package.py <package_version> <build_dir> <install_dir>"
         sys.exit(2)
 
     qinfo = QtInfo()
     py_version = "%s.%s" % (sys.version_info[0], sys.version_info[1])
-    create_package(sys.argv[1],
+    create_package("%s.%s" % (sys.argv[1], datetime.date.today().strftime('%Y%m%d')),
                    sys.argv[2],
                    sys.argv[3],
                    py_version,
                    qinfo,
-                   False) 
+                   False)
